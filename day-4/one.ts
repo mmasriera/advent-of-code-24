36c76bb5615586010b2efc35a7f98a21738b2fb8
mmaasriera <miquelmasriera@gmail.com>
2024-12-24 15:05:44 +0100

    check in one direction

--- day-4/one.ts
+++ day-4/one.ts
@@ -3,16 +3,40 @@ import { readInputByLines } from '../utils/index.ts';
 
 type Reports = string[][];
 
-const getPuzzle = (): string[][] => {
+const XMAS = 'XMAS';
+
+const checkPattern = (puzzle: string[], rowIdx: number, colIdx: number): boolean => {
+    const following = puzzle[rowIdx].slice(colIdx, colIdx + 4);
+
+    return following === XMAS;
+}
+
+const getOccurrences = (puzzle: string[]): number => {
+    let count = 0;
+
+    puzzle.forEach((row, rowIdx) => {
+        Array.from(row).forEach((cell, colIdx) => {
+            if (cell === XMAS[0]) { // matches "X" then check the next 4 chars in every direction
+                // const following = puzzle[rowIdx].slice(colIdx, colIdx + 4);
+
+                if (checkPattern(puzzle, rowIdx, colIdx)) {
+                    count += 1;
+                }
+            }
+        });
+    });
+
+    return count;
+};
+
+const getPuzzle = (): string[] => {
     return readInputByLines('./inputs/input-test.txt');
 }
 
 const main = (): number => {
     const puzzle = getPuzzle();
 
-    console.log('puzzle:', puzzle);
-
-    return 3;
+    return getOccurrences(puzzle);
 }
 
 console.log('result day 4, part 1:', main()); // 
