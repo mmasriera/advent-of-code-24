54eaf6d38e6d9fd1ac8cbea6b01d02fe338538bd
mmaasriera <miquelmasriera@gmail.com>
2024-12-24 15:30:41 +0100

    check diagonals

--- day-4/one.ts
+++ day-4/one.ts
@@ -5,10 +5,47 @@ type Reports = string[][];
 
 const XMAS = 'XMAS';
 
-const checkPattern = (puzzle: string[], rowIdx: number, colIdx: number): boolean => {
-    const following = puzzle[rowIdx].slice(colIdx, colIdx + 4);
+// const hasAdjacentMatch = (puzzle: string[][], startRow: number, startCol: number, increaseRow: number, increaseCol: number): boolean => {
+//     const row = startRow + increaseRow;
+//     const col = startCol + increaseCol;
 
-    return following === XMAS;
+//     if (puzzle[row]?.[col] === XMAS[0]) {
+//         return true;
+//     }
+
+//     return false;
+// }
+
+const positionCount = (puzzle: string[], rowIdx: number, colIdx: number): number => {
+    let count = 0; // there could be more than a match for this position
+
+    if (puzzle[rowIdx].slice(colIdx, colIdx + 4) === XMAS) { // right
+        count += 1; // 3
+    } 
+
+    if (puzzle[rowIdx].slice(colIdx - 3, colIdx + 1) === XMAS.split('').reverse().join('')) { // left
+        count += 1; // 5
+    }
+
+    if (puzzle[rowIdx]?.[colIdx] === XMAS[0] // down
+        && puzzle[rowIdx + 1]?.[colIdx] === XMAS[1]
+        && puzzle[rowIdx + 2]?.[colIdx] === XMAS[2]
+        && puzzle[rowIdx + 3]?.[colIdx] === XMAS[3]
+    ) {
+        count += 1; // 6
+    }
+
+    if (puzzle[rowIdx]?.[colIdx] === XMAS[0] // up
+        && puzzle[rowIdx - 1]?.[colIdx] === XMAS[1]
+        && puzzle[rowIdx - 2]?.[colIdx] === XMAS[2]
+        && puzzle[rowIdx - 3]?.[colIdx] === XMAS[3]
+    ) {
+        count += 1; // 8
+    }
+
+    // TO DO: check diagonals
+
+    return count;
 }
 
 const getOccurrences = (puzzle: string[]): number => {
@@ -16,12 +53,8 @@ const getOccurrences = (puzzle: string[]): number => {
 
     puzzle.forEach((row, rowIdx) => {
         Array.from(row).forEach((cell, colIdx) => {
-            if (cell === XMAS[0]) { // matches "X" then check the next 4 chars in every direction
-                // const following = puzzle[rowIdx].slice(colIdx, colIdx + 4);
-
-                if (checkPattern(puzzle, rowIdx, colIdx)) {
-                    count += 1;
-                }
+            if (cell === XMAS[0]) {
+                count += positionCount(puzzle, rowIdx, colIdx);
             }
         });
     });
