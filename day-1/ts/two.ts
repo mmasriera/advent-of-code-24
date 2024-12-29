28fade2302321570783a54b772827a45324ec121
mmaasriera <miquelmasriera@gmail.com>
2024-12-30 00:19:37 +0100

    biome-lint-and-format

--- day-1/ts/two.ts
+++ day-1/ts/two.ts
@@ -1,47 +1,46 @@
-
 import { readInputByLines } from '../../utils/index.ts';
 
 type Lists = [number[], number[]];
-type Counter = Record<number, number>
+type Counter = Record<number, number>;
 
 const getListsFromInput = (): Lists => {
-    const lists: Lists = [[], []];
+	const lists: Lists = [[], []];
 
-    readInputByLines('../inputs/input-one.txt')  // same as in part 1
-        .map(line => line.split('   ').map(Number))
-        .forEach(([first, second]) => {
-            lists[0].push(first);
-            lists[1].push(second);
-        });
+	readInputByLines('../inputs/input-one.txt') // same as in part 1
+		.map((line) => line.split('   ').map(Number))
+		.forEach(([first, second]) => {
+			lists[0].push(first);
+			lists[1].push(second);
+		});
 
-    return lists;
+	return lists;
 };
 
 const buildCounter = (list: number[]): Counter => {
-    return list.reduce((counter, id) => {
-        counter[id] = (counter[id] || 0) + 1;
+	return list.reduce((counter, id) => {
+		counter[id] = (counter[id] || 0) + 1;
 
-        return counter;
-    }, {} as Counter);
-}
+		return counter;
+	}, {} as Counter);
+};
 
 const calculateSimilarityScore = (list: number[], counter: Counter): number => {
-    let similarityScore = 0;
+	let similarityScore = 0;
 
-    list.forEach(id => {
-        const repetitions = counter[id] || 0;
+	list.forEach((id) => {
+		const repetitions = counter[id] || 0;
 
-        similarityScore += id * repetitions;
-    })
+		similarityScore += id * repetitions;
+	});
 
-    return similarityScore;
+	return similarityScore;
 };
 
 const main = (): number => {
-    const [list1, list2] = getListsFromInput();
-    const counter = buildCounter(list2);
-    
-    return calculateSimilarityScore(list1, counter);
+	const [list1, list2] = getListsFromInput();
+	const counter = buildCounter(list2);
+
+	return calculateSimilarityScore(list1, counter);
 };
 
 console.log('result part 2:', main()); // success: 23387399
