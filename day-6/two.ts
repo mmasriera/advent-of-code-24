15f17ee71bcba07d8d80e4e90274b4ea05e7557e
mmaasriera <miquelmasriera@gmail.com>
2025-01-03 00:02:37 +0100

    v2

--- day-6/two.ts
+++ day-6/two.ts
@@ -39,7 +39,30 @@ const rotate90degrees = (direction: number[]): number[] => {
 	return DIRECTIONS[(DIRECTIONS.indexOf(direction) + 1) % DIRECTIONS.length];
 };
 
+const printDirection = (direction: number[]): string => {
+	if (direction[0] === -1) {
+		return '^';
+	}
+
+	if (direction[1] === 1) {
+		return '>';
+	}
+
+	if (direction[0] === 1) {
+		return 'v';
+	}
+
+	return '<';
+};
+
+const printMap = (map: string[][]): void => {
+	console.log(map.map((row) => row.join('')).join('\n'));
+};
+
 const hasLoop = (map: string[][], startDirection: number[], start: Position, obstacle: Position): boolean => {
+	const mapCopy = map.map((row) => row.slice());
+	mapCopy[obstacle.row][obstacle.col] = 'O';
+	mapCopy[start.row][start.col] = 'S';
 	let direction = rotate90degrees(startDirection);
 	let rotations = 0;
 	let position = { ...start };
@@ -48,21 +71,25 @@ const hasLoop = (map: string[][], startDirection: number[], start: Position, obs
 		const [nextRow, nextCol] = [position.row + direction[0], position.col + direction[1]];
 
 		if (isOutOfMap(nextRow, nextCol, map)) {
+			// console.log(printMap(mapCopy));
 			return false;
 		}
 
 		if (rotations === 1000) {
+			console.log('-------- rotations', position, direction);
+			console.log(printMap(mapCopy));
+			return false;
+		}
+
+		if (nextRow === obstacle.row && nextCol === obstacle.col && direction[0] === startDirection[0] && direction[1] === startDirection[1]) {
 			return true;
 		}
 
-		if (nextRow === obstacle.row && nextCol === obstacle.col) {
-			return true;
-		}
-
-		if (isBlocker(nextRow, nextCol, map)) {
+		if (isBlocker(nextRow, nextCol, map) || mapCopy[nextRow][nextCol] === 'O') {
 			direction = rotate90degrees(direction);
 			rotations += 1;
 		} else {
+			mapCopy[nextRow][nextCol] = printDirection(direction);
 			position = { row: nextRow, col: nextCol };
 		}
 	}
@@ -83,7 +110,7 @@ const countLoops = (map: string[][]): number => {
 		if (isBlocker(nextRow, nextCol, map)) {
 			direction = rotate90degrees(direction);
 		} else {
-			if (hasLoop(map, direction, position, { row: nextRow, col: nextCol })) {
+			if (hasLoop(map, direction, position, { row: position.row - 1, col: position.col })) {
 				loops += 1;
 				console.log('loop found', { row: nextRow, col: nextCol }, loops);
 			}
@@ -96,7 +123,7 @@ const countLoops = (map: string[][]): number => {
 const main = (): void => {
 	const map = readInputByLines('./inputs/input-one.txt').map((line) => line.split(''));
 
-	console.log('result day 6, part 2:', countLoops(map)); // 1373 is too high
+	console.log('result day 6, part 2:', countLoops(map)); //1304
 
 };
 
