95a283f235b70a0ff943e83cb2f638d91c4e3edb
mmaasriera <miquelmasriera@gmail.com>
2025-01-04 15:50:53 +0100

    day 7 part 1

--- day-7/one.ts
+++ day-7/one.ts
@@ -1,15 +1,9 @@
 import { readInputByLines } from '../utils/index.ts';
 
-const OPERATIONS = [
-	// (a: number, b: number): number => a + b,
-	// (a: number, b: number): number => a * b,
-	// (a: number, b: number): number => a / b,
-	// (a: number, b: number): number => a - b,
-	'+',
-	'*',
-	'/',
-	'-',
-];
+const OPERATIONS = {
+	sum: (a: number, b: number): number => a + b,
+	mult: (a: number, b: number): number => a * b,
+}
 
 const buildCandidates = (input: string): [number, number[]] => {
 	const [left, right] = input.split(': ');
@@ -18,26 +12,20 @@ const buildCandidates = (input: string): [number, number[]] => {
 	return [Number(left), numbers];
 };
 
-const hasValidEquation = ([value, operands]: [number, number[]]): boolean => {
+const hasValidEquation = ([value, [first, second, ...rest]]: [number, number[]]): boolean => {
+	console.log('iteration', value, '=', first, second, rest);
 
-	console.log('-->', value);
+	const results = [
+		OPERATIONS.sum(first, second),
+		OPERATIONS.mult(first, second)
+	];
 
-	for (let i = 0; i < operands.length - 1; i += 1) {
-		for (let j = 0; j < Object.keys(OPERATIONS).length; j += 1) {
-			const operandsCopy = [...operands];
-			
-			let combination = '';
-
-			const last = operandsCopy.pop();
-			for (const operand of operandsCopy) {
-				combination += `${ operand } ${ OPERATIONS[(j + i) % 4] } `;
-			}
-
-			console.log(`${ value } = ${ combination }${ last }`);
-		}
+	if (rest.length === 0) {
+		return results.includes(value);
 	}
 
-	return false;
+	return hasValidEquation([value, [results[0], ...rest]])
+		|| hasValidEquation([value, [results[1], ...rest]])
 };
 
 const calibrationResult = (input: string[]): number => {
@@ -49,9 +37,9 @@ const calibrationResult = (input: string[]): number => {
 };
 
 const main = (): void => {
-	const input = readInputByLines('./inputs/input-test.txt');
+	const input = readInputByLines('./inputs/input-one.txt');
 
-	console.log('result day 7, part 1:', calibrationResult(input)); //
+	console.log('result day 7, part 1:', calibrationResult(input)); // 66343330034722
 };
 
 main();
