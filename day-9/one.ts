4357c7af7904ab40c5a8bc09f3f00a53735fbc50
mmaasriera <miquelmasriera@gmail.com>
2025-01-05 18:57:18 +0100

    v1

--- day-9/one.ts
+++ day-9/one.ts
@@ -18,14 +18,46 @@ const getBlocks = (diskMap: string): Block[] => {
 	return blocks;
 };
 
+const compact = (blocks: Block[]): Block[] => {
+	let i = 0;
+	let j = blocks.length - 1;
+
+	while (i < j) {
+		// console.log(counter, '--> blocks', blocks, i, j);
+
+		if (blocks[i].space > 0) {
+			const last = blocks[j].file.slice(-1);
+	
+			if (last) {
+				blocks[i].file += last;
+				blocks[i].space -= 1;
+	
+				blocks[j].file = blocks[j].file.slice(0, -1);
+				blocks[j].space += 1;
+			} else {
+				j -= 1;
+			}
+		} else {
+			i += 1;
+		}
+	}
+
+	return blocks;
+};
+
 // opt 1: build a representation and move 2 pointers
 // opt 2: use a structure to keep track of the free space and the blocks
 
 const main = (): void => {
 	const diskMap = readInputByLines('./inputs/test.txt')[0]; // may need to read as stream (1 line of 19999 chars)
 	const blocks = getBlocks(diskMap);
+	const result = compact(blocks).reduce((acc, block) => acc + block.file, '');
 
-	console.log('result day 9, part 1:', blocks); //
+	const checkSum = Array.from(result).reduce((acc, char, idx) => {
+		return acc + Number(char) * idx;
+	}, 0);
+
+	console.log('result day 9, part 1:', checkSum); //
 };
 
 main();
