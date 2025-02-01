bab59e4fa98478fdc966179bcf51dc94f3e454af
miki mq <miquelmasriera@gmail.com>
2025-02-01 16:17:47 +0100

    9 two (#19)
    * 9 two
    
    * 9 2 tmp
    
    * working
    
    * answer

--- day-9/two.ts
+++ day-9/two.ts
@@ -1,3 +1,4 @@
+
 import { readInputByLines } from '../utils/index.ts';
 
 type freeBlock = {
@@ -5,7 +6,7 @@ type freeBlock = {
 	size: number
 }
 
-const makeDisk = (line: string): { disk: number[]; freeIndexes: number[] } => {
+const makeDisk = (line: string): { disk: number[]; freeIndexes: freeBlock[] } => {
 	const disk = [];
 	const freeIndexes = [];
 	let id = 0;
@@ -17,9 +18,7 @@ const makeDisk = (line: string): { disk: number[]; freeIndexes: number[] } => {
 			disk.push(...blocks.fill(id));
 			id += 1;
 		} else {
-			for (let idx = disk.length; idx < disk.length + blocks.length; idx += 1) {
-				freeIndexes.push(idx);
-			}
+			freeIndexes.push({ idx: disk.length, size: blocks.length });
 			disk.push(...blocks.fill(-1)); // free space
 		}
 	}
@@ -27,28 +26,52 @@ const makeDisk = (line: string): { disk: number[]; freeIndexes: number[] } => {
 	return { disk, freeIndexes };
 };
 
-const compact = (disk: number[], freeIndexes: number[]): number[] => {
-	for (const freeIndex of freeIndexes) {
-		while (disk[disk.length - 1] === -1) {
-			disk.pop();
+const compact = (disk: number[], freeIndexes: freeBlock[]): number[] => {
+	let z = disk.length - 1; // start iterating from the end
+
+	while (z > 0) {
+		const value = disk[z];
+		const block = [];
+
+		if (value === -1) {
+			z -= 1;
+			continue;
 		}
 
-		if (disk.length > freeIndex) {
-			// biome-ignore lint/style/noNonNullAssertion: pop will always return a value
-			disk[freeIndex] = disk.pop()!;
+		while (disk[z] === value) {
+			block.push(disk[z]);
+			z -= 1;
+		}
+
+		// fit lastblock in freeIndexes
+		for (const freeIndex of freeIndexes) {
+			if (freeIndex.size >= block.length && (freeIndex.idx < z)) {
+				for (let idx = 0; idx < block.length; idx += 1) {
+					disk[freeIndex.idx + idx] = block[idx]; // place element of block in a free space
+					disk[z + idx + 1] = -1; // free from the block at the end
+				}
+
+				freeIndex.size -= block.length;
+				freeIndex.idx += block.length;
+
+				break;
+			}
 		}
 	}
+	
 
 	return disk;
-};
+}
 
 const main = (): void => {
-	const line = readInputByLines('./inputs/test.txt')[0];
+	const line = readInputByLines('./inputs/input.txt')[0];
 	const { disk, freeIndexes } = makeDisk(line);
-	// const compacted = compact(disk, freeIndexes);
-	// const checksum = compacted.reduce((acc, curr, idx) => acc + curr * (idx), 0);
+	const compacted = compact(disk, freeIndexes);
+	const checksum = compacted
+		.map(value => value === -1 ? 0 : value)
+		.reduce((acc, curr, idx) => acc + curr * (idx), 0);
 
-	console.log('result day 9, part 1:', freeIndexes); // 6283170117911
+	console.log('result day 9, part 2:', checksum); // 6307653242596
 };
 
 main();
