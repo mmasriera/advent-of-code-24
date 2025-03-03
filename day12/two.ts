42dc09e4c831bc438cf36d806f6dc96c3863b277
mmaasriera <miquelmasriera@gmail.com>
2025-03-03 20:58:56 +0100

    map positions

--- day12/two.ts
+++ day12/two.ts
@@ -1,4 +1,4 @@
-import { readInputByLines, DIRECTIONS, type Position } from '../utils/index.ts';
+import { readInputByLines, MAP_DIRECTIONS, type MapPosition, getCoordinate } from '../utils/index.ts';
 
 /*
 	BFS + visited
@@ -7,32 +7,30 @@ import { readInputByLines, DIRECTIONS, type Position } from '../utils/index.ts';
 const VISITED = new Set<string>();
 
 type Edge = {
-	position: Position,
+	position: MapPosition,
 	direction: string // will be the index of the direction
 }
 
-const posToId = (position: Position): string => `${position.x},${position.y}`;
-
-const calculateRegionCost = (map: string[][], char: string, position: Position): number => {
-	const candidates: Position[] = [position]; // candidates for the region (the 1st one will always be)
+const calculateRegionCost = (map: string[][], char: string, position: MapPosition): number => {
+	const candidates: MapPosition[] = [position]; // candidates for the region (the 1st one will always be)
 	const region = new Set<string>();
 	const edges: Edge[] = [];
 
-	region.add(posToId(position));
+	region.add(getCoordinate(position));
 
 	while (candidates.length > 0) {
 		const candidate = candidates.shift(); // get the 1st one
 
-		for (const increment of DIRECTIONS) {
+		for (const increment of MAP_DIRECTIONS) {
 			// biome-ignore lint/style/noNonNullAssertion: the while condition prevents candidate from being undefined
-			const next = { x: candidate!.x + increment.x, y: candidate!.y + increment.y };
-			const nextCoordinate = posToId(next);
+			const next = { row: candidate!.row + increment.row, col: candidate!.col + increment.col };
+			const nextCoordinate = getCoordinate(next);
 
 			if (region.has(nextCoordinate)) {
 				continue;
 			}
 
-			if (map[next.x]?.[next.y] === char) {
+			if (map[next.row]?.[next.col] === char) {
 				region.add(nextCoordinate);
 				VISITED.add(nextCoordinate);
 				candidates.push(next);
@@ -40,19 +38,19 @@ const calculateRegionCost = (map: string[][], char: string, position: Position):
 				edges.push({
 					// biome-ignore lint/style/noNonNullAssertion: the while condition prevents candidate from being undefined
 					position: candidate!,
-					direction: posToId(increment)
+					direction: getCoordinate(increment)
 				});
 			}
 		}
 	}
 
-	console.log('region', { region });
+	console.log('region', { region, edges });
 
 	// to do: calculate the actual sides
 	/*
 		foreach direction -> findall --> sort by x/y --> check them all and +1 for every non continous
 
-		alt: for each one -> find contigous --> remove themf from the edges list
+		alt: for each one -> find contigous --> remove them from the edges list
 
 		altBetter: when saving edges --> instead of direction, save range, update range / add new
 	*/
@@ -65,9 +63,9 @@ const calculateTotalCost = (map: string[][]): number => {
 	let result = 0;
 
 	for (let row = 0; row < map.length; row += 1) {
-		for (let column = 0; column < map[row].length; column += 1) {
-			const coordinate = `${row},${column}`;
-			const char = map[row][column];
+		for (let col = 0; col < map[row].length; col += 1) {
+			const coordinate = `${row},${col}`;
+			const char = map[row][col];
 
 			if (VISITED.has(coordinate)) {
 				continue;
@@ -75,7 +73,7 @@ const calculateTotalCost = (map: string[][]): number => {
 
 			VISITED.add(coordinate);
 
-			result += calculateRegionCost(map, char, { x: row, y: column });
+			result += calculateRegionCost(map, char, { row, col });
 		}
 	}
 
