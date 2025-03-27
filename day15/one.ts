90f023e4b1d266dd9f560fb1733ff28960f732f8
mmaasriera <miquelmasriera@gmail.com>
2025-03-27 20:45:36 +0100

    15 1

--- day15/one.ts
+++ day15/one.ts
@@ -1,21 +1,30 @@
 import { readInputByLines } from '../utils/index.ts';
+import { MAP_DIRECTIONS, type MapPosition, findInMap, sumPositions } from '../utils/index.ts';
 
 type Input = {
-	map: string[];
+	map: string[][];
 	movements: string;
-}
+};
 
 const WALL = '#';
 const ROBOT = '@';
 const BOX = 'O';
+const EMPTY = '.';
+
+const MOVEMENT_DIRECTIONS = {
+	'<': MAP_DIRECTIONS[3],
+	'>': MAP_DIRECTIONS[1],
+	'^': MAP_DIRECTIONS[0],
+	v: MAP_DIRECTIONS[2],
+};
 
 const parseInput = (lines: string[]): Input => {
-	const map: string[] = [];
+	const map: string[][] = [];
 	let movements = '';
 
 	for (const line of lines) {
 		if (line.startsWith(WALL)) {
-			map.push(line);
+			map.push(line.split(''));
 		} else {
 			movements = movements.concat(line);
 		}
@@ -24,10 +33,79 @@ const parseInput = (lines: string[]): Input => {
 	return { map, movements };
 };
 
-const main = (): void => {
-	const { map, movements } = parseInput(readInputByLines('./inputs/test.txt'));
+const sumGpsCoordinates = (map: string[][]): number => {
+	let result = 0;
 
-	console.log('result day 15, part 1:', map, movements); // 
+	for (let row = 0; row < map.length; row++) {
+		for (let col = 0; col < map[row].length; col++) {
+			if (map[row][col] === BOX) {
+				result += row * 100 + col;
+			}
+		}
+	}
+
+	return result;
+};
+
+const pushBox = (map: string[][], initialBoxPosition: MapPosition, direction: MapPosition): boolean => {
+	const next = sumPositions(initialBoxPosition, direction);
+
+	if (map[next.row][next.col] === BOX) {
+		return pushBox(map, next, direction);
+	}
+
+	if (map[next.row][next.col] === EMPTY) {
+		map[next.row][next.col] = BOX;
+
+		return true;
+	}
+
+	return false;
+};
+
+const doMovement = (map: string[][], current: MapPosition, movement: string): MapPosition => {
+	const direction = MOVEMENT_DIRECTIONS[movement as keyof typeof MOVEMENT_DIRECTIONS];
+	const next = sumPositions(current, direction);
+
+	if (map[next.row][next.col] === EMPTY) {
+		return next;
+	}
+
+	if (map[next.row][next.col] === BOX) {
+		const canPush = pushBox(map, next, direction);
+
+		if (canPush) {
+			map[next.row][next.col] = EMPTY;
+
+			return next;
+		}
+	}
+
+	return current; // wall or can not push
+};
+
+const moveBoxes = (map: string[][], movements: string): void => {
+	let robot = findInMap(map, ROBOT);
+
+	if (!robot) {
+		return;
+	}
+
+	map[robot.row][robot.col] = EMPTY;
+
+	for (const movement of movements) {
+		robot = doMovement(map, robot, movement);
+	}
+};
+
+const main = (): void => {
+	const { map, movements } = parseInput(readInputByLines('./inputs/main.txt'));
+
+	moveBoxes(map, movements); // updates map
+
+	const result = sumGpsCoordinates(map);
+
+	console.log('result day 15, part 1:', result); // 1509863
 };
 
 main();
